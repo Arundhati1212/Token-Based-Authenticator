d4277d170faa7638062c010bbe8e5a91f20f7193
Arundhati Gadge <51483255+Arundhati1212@users.noreply.github.com>
2024-05-27 01:39:49 -0700

    Create README.m

---
+++ README.m
@@ -0,0 +1,37 @@
+Authenticated Event Hub
+As a frontend developer dedicated to creating secure and intuitive web applications, I'm thrilled to introduce my latest project: Authenticated Event Hub. This project highlights my learning in implementing robust authentication systems and seamless navigation using React routing.
+Project Overview
+Event Blog: The Event Blog is a comprehensive web application enabling users to securely sign up, log in, and manage events. Leveraging React.js for frontend development and integrating a backend authentication service, this project prioritizes user authentication and authorization while providing seamless navigation through React routing.
+Key Features and Technologies Utilized
+Secure Authentication: Implemented a secure authentication system using a backend service to securely store user signup details and manage user sessions. Utilized token-based authentication, with tokens generated upon successful login and sent in request headers for authorization, ensuring access to protected routes and actions.
+Token Expiration Handling: Implemented robust token expiration handling to automatically log users out after a specified period, enhancing security and protecting user accounts from unauthorized access.
+React Routing: Leveraged React Router for efficient client-side routing, enabling seamless navigation between different pages and ensuring a smooth user experience.
+
+Why Authenticated Event Hub Stands Out
+Authenticated Event Hub is more than just a project; it's a demonstration of my commitment to mastering secure authentication methods and delivering user-friendly web applications. Through this project, I have refined my skills in React.js and gained valuable experience in creating real-world applications that prioritize user experience and security.
+
+How to Install and Run the Project
+To run MealMate locally, follow these steps:
+1.	Clone the repository: git clone https://github.com/Arundhati1212/Token-Based-Authenticator.git
+2.	Navigate to the project directory:
+3.	Install dependencies: npm install 
+4.	Start the development server: 
+    /backend : npm start
+    / : npm run dev 
+The application should now be running on http://localhost:5173.
+
+
+How to Use the Project
+1.	Sign Up:
+•	Navigate to the signup page and create a new account.
+2.	Log In:
+•	Use your credentials to log in and access your account.
+3.	Manage Events:
+•	Create, view, and manage your events through the user-friendly interface.
+
+
+Contributors
+•	Arundhati Gadge - linkedin.com/in/arundhati-gadge/
+
+
+
